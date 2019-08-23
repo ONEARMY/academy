33d6fc21cc3d32480da4d482f334ecf47e715551
chrismclarke <chris.m.clarke@live.co.uk>
2019-08-23 15:19:15 +0000

    Deploy website
    Deploy website version based on 354d26a8d49bf12d264014072aa19b8872498f6d

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,20 @@
 window.addEventListener("load", function() {
-  var links = document.querySelectorAll(".navGroups .navGroup .collapsible");
-  var l = Array.from(links);
-  links.forEach(function(el) {
-    el.addEventListener("click", function() {
+  var categoryNodes = document.querySelectorAll(
+    ".navGroups .navGroup .collapsible"
+  );
+  var categoryLinks = Array.from(categoryNodes);
+  categoryNodes.forEach(function(category) {
+    category.addEventListener("click", function() {
       var that = this;
-      l.filter(function(_, i) {
-        var index = l.indexOf(that);
-        return i !== index;
-      }).forEach(function(link) {
-        link.parentNode.querySelector("ul").classList.add("hide");
-      });
+      categoryLinks
+        .filter(function(_, i) {
+          var index = categoryLinks.indexOf(that);
+          return i !== index;
+        })
+        .forEach(function(link) {
+          link.parentNode.querySelector("ul").classList.add("hide");
+        });
+      that.parentNode.querySelector("ul .navListItem a").click();
     });
   });
 });
