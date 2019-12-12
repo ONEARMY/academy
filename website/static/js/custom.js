264a36e535bdbc630ff56dd4ea0dc76f769d441b
Martin Sjåstad <m.sjastad@gmail.com>
2019-12-13 00:24:17 +0100

    Add second level of sub chapters

--- website/static/js/custom.js
+++ website/static/js/custom.js
@@ -1,3 +1,7 @@
+function addHideClass(item) {
+  return item.classList.add("hide");
+}
+
 window.addEventListener("load", function() {
   var categoryNodes = document.querySelectorAll(
     ".navGroups .navGroup .collapsible"
@@ -18,14 +22,36 @@ window.addEventListener("load", function() {
     });
   });
 
-  var slideshow = tns({
-    container: ".j-slideshow",
-    items: 1,
-    controlsPosition: "bottom",
-    navPosition: "bottom",
-    slideBy: "page",
-    controlsText: ["<", ">"]
+  var subNodes = document.querySelectorAll(
+    ".navGroups .navGroup.subNavGroup .navGroupSubcategoryTitle"
+  );
+  //   var subCatLinks = Array.from(subNodes);
+  subNodes.forEach(function(category) {
+    category.classList.add("collapsible");
+    category.parentNode.querySelectorAll("ul").forEach(addHideClass);
+    category.addEventListener("click", function() {
+      var that = this;
+      that.parentNode.querySelector("ul").classList.toggle("hide");
+      //   subCatLinks
+      //     .filter(function(_, i) {
+      //       var index = subCatLinks.indexOf(that);
+      //       return i !== index;
+      //     })
+      //     .forEach(function(link) {
+      //       link.parentNode.querySelector("ul").classList.add("hide");
+      //     });
+      //   that.parentNode.querySelector("ul .navListItem a").click();
+    });
   });
+
+  //   var slideshow = tns({
+  //     container: ".j-slideshow",
+  //     items: 1,
+  //     controlsPosition: "bottom",
+  //     navPosition: "bottom",
+  //     slideBy: "page",
+  //     controlsText: ["<", ">"]
+  //   });
 });
 // On every page load emit the current pathname
 // for use in iframe embeds
